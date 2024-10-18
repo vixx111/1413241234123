851d91bb8ef88002f4c29390dc80176fe3083714
Admin <Admin@DESKTOP-UGP0C2G>
2024-10-18 20:33:27 +0200

    Добавьте файлы проекта.

---
+++ 1413241234123/Program.cs
@@ -0,0 +1,14 @@
+﻿try
+{
+    Console.WriteLine("Введите сумму");
+    Double sum = Double.Parse(Console.ReadLine());
+    Console.WriteLine("Введите срок");
+    Double years = Double.Parse(Console.ReadLine());
+    Double stavka = 8 / 100;
+    Double j = Math.Sqrt(sum) * Math.Sqrt(stavka) * Math.Sqrt(years);
+    Console.WriteLine($"Итог: {j}");
+}
+catch
+{
+
+}
